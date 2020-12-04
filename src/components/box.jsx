9a52fa8abe11722b732ed052662c216ed5d7b581
Qalac <officialqalac@gmail.com>
2020-12-04 21:12:05 +0000

    initial commit

---
+++ src/components/box.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './box.css';
+
+class Box extends React.Component {
+    render() {
+        return(
+            <div className="box">
+                <div className="counter">
+                    COUNTER
+                </div>
+                ----------------------------------
+                <div>
+                    <button className="add">+</button>
+                    <span className="count"> 2 </span>
+                    <button className="subtract">-</button>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Box;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Box from './components/box';
+
+const App = () => (
+    <div>
+        <Box />
+    </div>
+);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
