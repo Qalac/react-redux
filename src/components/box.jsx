2da2fb7ada72744078239ce2421db604331d5ac1
Qalac <officialqalac@gmail.com>
2020-12-04 23:51:45 +0000

    added counter

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 import './box.css';
 
 class Box extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleSubtract = this.handleSubtract.bind(this);
+    }
+
+    handleAdd() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    handleSubtract() {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
     render() {
         return(
             <div className="box">
@@ -10,9 +31,9 @@ class Box extends React.Component {
                 </div>
                 ------------------------------------
                 <div>
-                    <button className="add">+</button>
-                    <span className="count"> 2 </span>
-                    <button className="subtract">-</button>
+                    <button className="add" onClick={this.handleAdd}>+</button>
+                    <span className="count"> {this.state.count} </span>
+                    <button className="subtract" onClick={this.handleSubtract}>-</button>
                 </div>
             </div>
         );
